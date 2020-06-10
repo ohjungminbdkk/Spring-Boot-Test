aadc989944a9e84f201a8859a8cb216798faed67
bdkk0 <bdkk0@192.168.2.107>
2020-06-10 22:46:02 +0900

    [BOOK-TEST] SpringBootテスト

---
+++ src/test/java/com/havi/SpringBootTestApplicationTests.java
@@ -0,0 +1,37 @@
+package com.havi;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+//@SpringBootTest(value = "value=test", properties = { "property.value=propertyTest" }, classes = {
+//		SpringBootTestApplication.class }, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@SpringBootTest(value = "value=test", classes = {
+//		SpringBootTestApplication.class }, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(properties = { "property.value=propertyTest" }, classes = {
+		SpringBootTestApplication.class }, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class SpringBootTestApplicationTests {
+	/* @SpringBootTest
+	 * 안에 value 와 properties 중 한개의 형식으로 만사용가능하다.
+	 * 
+	 */	
+	
+//	@Value("${value}")
+//	private String value;
+
+	@Value("${property.value}")
+	private String propertyValue;
+
+	@Test
+	public void contextLoads() {
+//		assertThat(value, is("test"));
+		assertThat(propertyValue, is("propertyTest"));
+	}
+
+}
