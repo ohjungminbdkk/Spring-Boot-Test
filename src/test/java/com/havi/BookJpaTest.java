812f63d2b49d57783d24a5f81bb90f68c2a0864f
bdkk0 <bdkk0@192.168.2.107>
2020-06-12 23:33:43 +0900

    [BOOK-TEST] SpringBootテスト(BOOK_JPA_SUCCESS)

--- src/test/java/com/havi/BookJpaTest.java
+++ src/test/java/com/havi/BookJpaTest.java
@@ -6,6 +6,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.hamcrest.collection.IsEmptyCollection;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -63,6 +64,24 @@ public class BookJpaTest {
 		
 		assertThat(bookList2, hasSize(3)); 
 
+		// 삭제 테스트
+		bookRepository.deleteAll();
+		
+		assertThat(bookRepository.findAll(), IsEmptyCollection.empty()); 
 	}
+	
+	@Test
+	public void BookList_저장하고_삭제_테스트() {
+		Book book1 = Book.builder().title(BOOT_TEST_TITLE + "1").publishedAt(LocalDateTime.now()).build();
+		testEntityManager.persist(book1);
+		Book book2 = Book.builder().title(BOOT_TEST_TITLE + "2").publishedAt(LocalDateTime.now()).build();
+		testEntityManager.persist(book2);
+		
+		bookRepository.deleteAll();
+		
+		assertThat(bookRepository.findAll(), IsEmptyCollection.empty()); 
+
+	}
+	
 
 }
